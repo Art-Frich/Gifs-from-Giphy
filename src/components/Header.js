8c6a06f5ab0ea28f68e8225ead984ba917de0b87
Art Frich <frich.g23@yandex.ru>
2023-07-01 01:29:53 +0500

    feat: create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,45 @@
+import './Header.css';
+import logo from '../images/logo.svg';
+import { NavLink } from 'react-router-dom';
+
 export default function Header(){
-  
+  return(
+    <header className="header">
+      <img className="header__logo" src={ logo } alt='logo' />
+      <nav>
+        <ul className="header__links">
+        <li>
+            <NavLink
+              to="/search"
+              className="header__link"
+              activeClassName="header__link_active"
+              // onClick={() => handleLinkClick('Поиск')}
+            >
+              Поиск
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/trends"
+              className="header__link"
+              activeClassName="header__link_active"
+              // onClick={() => handleLinkClick('Тренды')}
+            >
+              Тренды
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/random"
+              className="header__link"
+              activeClassName="header__link_active"
+              // onClick={() => handleLinkClick('Случайный гиф')}
+            >
+              Случайный гиф
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
