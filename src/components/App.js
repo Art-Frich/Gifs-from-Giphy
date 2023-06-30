4c05217e6a3e2d86ba385d49a70334c76f61bf77
Art Frich <frich.g23@yandex.ru>
2023-07-01 01:29:44 +0500

    feat: create header

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,12 @@ import Api from '../utils/Api';
 function App() {
   return <>
     <Header />
-      <Routes>
-        {/* <Route /> гифки по запросу*/}
-        {/* <Route /> гифки в тренде */}
-        {/* <Route /> случайная гифка*/}
-      </Routes>
+    <Routes>
+      {/* <Route /> гифки по запросу*/}
+      {/* <Route /> гифки в тренде */}
+      {/* <Route /> случайная гифка*/}
+      {/* <Route to='*' переброс с любого невалидного адреса /> */}
+    </Routes>
     <Footer />
   </>;
 }
