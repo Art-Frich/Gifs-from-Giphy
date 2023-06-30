2bf4ff6c1613fb331be279a165faa0fa1dc1667c
Art Frich <frich.g23@yandex.ru>
2023-07-01 01:42:50 +0500

    feat: create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,13 @@
+import './Footer.css';
+
 export default function Footer(){
-  
+  return (
+    <footer className="footer">
+      <div className="footer__block">
+        <span className="footer__authors">
+        © 2023 Art Frich & Denis Cherezov
+        </span>
+      </div>
+    </footer>
+  )
 }
